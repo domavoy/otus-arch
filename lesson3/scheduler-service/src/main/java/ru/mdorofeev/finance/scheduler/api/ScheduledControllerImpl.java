9e2a1c2c696163e5368aa44da5441c4bddf1f4ac
Dorofeev Maxim <mdorofeev@vsk.ru>
2020-02-02 13:16:15 +0300

    [lesson 3] scheduler-service - create scheduler

--- lesson3/scheduler-service/src/main/java/ru/mdorofeev/finance/scheduler/api/ScheduledControllerImpl.java
+++ lesson3/scheduler-service/src/main/java/ru/mdorofeev/finance/scheduler/api/ScheduledControllerImpl.java
@@ -3,11 +3,10 @@ package ru.mdorofeev.finance.scheduler.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.mdorofeev.finance.common.api.Processor;
-import ru.mdorofeev.finance.common.api.model.response.ErrorResponse;
 import ru.mdorofeev.finance.common.api.model.response.Response;
-import ru.mdorofeev.finance.common.exception.ServiceException;
 import ru.mdorofeev.finance.scheduler.service.ScheduledService;
 
 
@@ -17,6 +16,7 @@ public class ScheduledControllerImpl implements ScheduledController {
     @Autowired
     ScheduledService scheduledService;
 
+    @Scheduled(cron = "${app.scheduling.currencyUpdateCron}")
     @Override
     public ResponseEntity<Response> executeCurrencyUpload() {
         return Processor.wrapExceptions(() -> {
@@ -25,6 +25,7 @@ public class ScheduledControllerImpl implements ScheduledController {
         });
     }
 
+    @Scheduled(cron = "${app.scheduling.createRepeatedPaymentCron}")
     @Override
     public ResponseEntity<Response> executeCreateRepeatedPayment() {
         return Processor.wrapExceptions(() -> {
